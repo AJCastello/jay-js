2d41d607e6469386fee45af93e995c4436e5a02c
AJ Castello <ajcastello1@gmail.com>
2023-05-23 17:00:55 -0400

    feat badge & box

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -12,6 +12,8 @@ export * from "./utils";
 
 export * from "./Alert";
 export * from "./BaseElement";
+export * from "./Badge";
+export * from "./Box";
 export * from "./Button";
 export * from "./Card";
 export * from "./CheckBox";
