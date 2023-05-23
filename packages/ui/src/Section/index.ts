c2b4ebdb9f1c80ce3614e1311cebd69557d9c533
AJ Castello <ajcastello1@gmail.com>
2023-05-23 16:59:01 -0400

    semantic tags as variant propertie

--- packages/ui/src/Section/index.ts
+++ packages/ui/src/Section/index.ts
@@ -1,15 +1,18 @@
 import { BaseElement, IBaseElement } from "../BaseElement";
 
-export type ISection = IBaseElement
+export interface ISection extends IBaseElement {
+  variant?: "header" | "footer" | "main" | "aside" | "section" | "article" | "nav" | "div" ;
+}
 
 export function Section({ 
   id, 
   content,
+  variant = "div",
   ...props
 }: ISection = {}): HTMLDivElement {
   const section = BaseElement({
     id,
-    tag: "div",
+    tag: variant || "div",
     ...props
   }) as HTMLDivElement;
 
