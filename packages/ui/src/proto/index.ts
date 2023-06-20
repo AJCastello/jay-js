7e23dad1484e7b51710fe8caae512db38c630b3d
AJ Castello <ajcastello1@gmail.com>
2023-06-20 18:44:42 -0400

    remove children

--- packages/ui/src/proto/index.ts
+++ packages/ui/src/proto/index.ts
@@ -1,6 +1,6 @@
 interface HTMLElement {
   content: string | HTMLElement | Node | DocumentFragment | (string | HTMLElement | Node)[];
-  children: string | HTMLElement | Node | DocumentFragment | HTMLCollection | (string | HTMLElement | Node)[];
+  // children: string | HTMLElement | Node | DocumentFragment | HTMLCollection | (string | HTMLElement | Node)[];
 }
 
 Object.defineProperty(HTMLElement.prototype, "content", {
@@ -29,28 +29,28 @@ Object.defineProperty(HTMLElement.prototype, "content", {
   configurable: true
 });
 
-Object.defineProperty(HTMLElement.prototype, "children", {
-  get: function getChildren() {
-    if (!this.children) return null;
-    return this.children as HTMLCollection;
-  },
-  set: function setContent(value) {
+// Object.defineProperty(HTMLElement.prototype, "children", {
+//   get: function getChildren() {
+//     if (!this.children) return null;
+//     return this.children as HTMLCollection;
+//   },
+//   set: function setContent(value) {
 
-    if (typeof this.append === "function") {
-      this.innerHTML = "";
-      if (Array.isArray(value)) {
-        value.forEach((item) => {
-          this.append(item);
-        });
-      } else {
-        value && this.append(value);
-      }
-    } else {
-      // in case the user tries to set the content of an element that doesn't support it
-      // (e.g., <input> or <img>)
-      console.warn("JayJS: Cannot set property 'children' of type 'string' to 'value'.");
-    }
-  },
-  enumerable: true,
-  configurable: true
-});
+//     if (typeof this.append === "function") {
+//       this.innerHTML = "";
+//       if (Array.isArray(value)) {
+//         value.forEach((item) => {
+//           this.append(item);
+//         });
+//       } else {
+//         value && this.append(value);
+//       }
+//     } else {
+//       // in case the user tries to set the content of an element that doesn't support it
+//       // (e.g., <input> or <img>)
+//       console.warn("JayJS: Cannot set property 'children' of type 'string' to 'value'.");
+//     }
+//   },
+//   enumerable: true,
+//   configurable: true
+// });
