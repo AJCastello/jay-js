5a1e3441adc933c83d915a95c85937762fe5bb5e
AJ Castello <ajcastello1@gmail.com>
2023-07-28 15:19:11 -0400

    clear function to state and instant run param to subscription listeners

--- packages/system/src/state/index.ts
+++ packages/system/src/state/index.ts
@@ -1,9 +1,10 @@
 export type StateType<T> = {
   set: (newData: T | ((currentState: T) => T)) => void;
   get: (callback?: (data: T) => void) => T;
-  sub: (id: string, effect: (data: T) => void) => any;
+  sub: (id: string, effect: (data: T) => void, run?: boolean) => void;
   unsub: (id: string) => void;
   trigger: () => void;
+  clear: (newData: T | ((currentState: T) => T)) => void;
   effect: Map<string, (data: T) => void>;
   data: T;
 };
@@ -28,8 +29,11 @@ export const State = <T>(data: T): StateType<T> => {
       }
       return data;
     },
-    sub: (id: string, effect: (data: T) => void) => {
+    sub: (id: string, effect: (data: T) => void, run: boolean = false) => {
       state.effect.set(id, effect);
+      if (run) {
+        effect(data);
+      }
     },
     unsub: (id: string) => {
       state.effect.delete(id);
@@ -39,6 +43,14 @@ export const State = <T>(data: T): StateType<T> => {
         return;
       }
       state.effect.forEach((item: (arg0: T) => any) => item(data));
+    },
+    clear: (newData: T | ((currentState: T) => T)): void => {
+      if (typeof newData === "function") {
+        data = (newData as (currentState: T) => T)(data);
+      } else {
+        data = newData;
+      }
+      state.effect.clear();
     },
     effect: new Map(),
     data,
