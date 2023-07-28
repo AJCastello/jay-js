f036903d967b9e82cf2b57d816f1a341c5994df1
AJ Castello <ajcastello1@gmail.com>
2023-07-28 15:25:47 -0400

    add content

--- packages/ui/src/Link/index.ts
+++ packages/ui/src/Link/index.ts
@@ -3,7 +3,6 @@ import { BaseElement, IBaseElement } from "..";
 export interface ILink extends IBaseElement {
   id?: string;
   href?: string;
-  content: string;
   className?: string;
 }
 
@@ -13,8 +12,15 @@ export function Link({ id, href, content, ...props }: ILink): HTMLAnchorElement 
     ...props,
     className: `link ${props.className ? props.className : ""}`,
   }) as HTMLAnchorElement;
+  
+  if (Array.isArray(content)) {
+    content.forEach((item) => {
+      link.append(item);
+    });
+  } else {
+    content && link.append(content);
+  }
 
-  link.innerText = content;
   href && (link.href = href);
 
   return link;
