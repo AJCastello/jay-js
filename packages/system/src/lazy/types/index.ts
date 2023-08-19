1032182437f529445b28ded5a208ce8c61d8020a
AJ Castello <ajcastello1@gmail.com>
2023-08-19 01:25:07 -0400

    remove route from interface

--- packages/system/src/lazy/types/index.ts
+++ packages/system/src/lazy/types/index.ts
@@ -7,7 +7,6 @@ export interface IImportedModule {
 export interface ILazyModule {
   module: string;
   import: () => Promise<any>;
-  route?: string;
   style?: any;
   props?: any;
   collect?: boolean;
