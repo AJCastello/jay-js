f0ec7cbad47bb4a7100b94bcd7fd76ef1adbdaff
AJ Castello <ajcastello1@gmail.com>
2023-10-04 15:03:24 -0400

    refactor drawer component and moving hook

--- packages/ui/src/components/Drawer/Drawer.ts
+++ packages/ui/src/components/Drawer/Drawer.ts
@@ -11,7 +11,7 @@ export function Drawer({
   asChild = false,
   position = "left",
   ...props
-}: IDrawer): HTMLElement | string {
+}: IDrawer): HTMLElement {
 
   const positionClass = {
     left: "justify-start",
@@ -21,7 +21,7 @@ export function Drawer({
   };
 
   const className = mergeClasses([
-    "fixed",
+    asChild ? "absolute" : "fixed",
     "inset-0",
     "flex",
     "hidden",
@@ -37,65 +37,10 @@ export function Drawer({
 
   const drawerId = drawer.id;
 
-  if (!asChild) {
-    const drawerElement = document.querySelector(`#${drawerId}`);
-    if (!drawerElement) {
-      document.body.appendChild(drawer);
-      return "";
-    }
-    return "";
+  if (document.querySelector(`#${drawerId}`)) {
+    const drawerElement = document.querySelector(`#${drawerId}`) as HTMLDivElement;
+    return drawerElement;
   }
 
   return drawer;
-}
-
-interface IUseDrawerToggle {
-  for?: string;
-}
-
-export function useDrawerToggle({
-  ...props
-}: IUseDrawerToggle) {
-
-  return function () {
-    const drawer = document.querySelector(`#${props.for}`);
-    if (drawer && drawer instanceof HTMLElement) {
-      const drawerOverlay = document.querySelector(`[data-drawer-for="${props.for}"]`);
-      const drawerContent = drawer.querySelector(".drawer-content");
-
-      if(drawer.classList.contains("hidden")) {
-        drawer.classList.remove("hidden");
-      } else {
-        setTimeout(() => {
-        drawer.classList.add("hidden");
-        }, 300);
-      }
-      
-      setTimeout(() => {
-        if (drawerContent && drawerContent instanceof HTMLElement) {
-
-          if (drawerContent.classList.contains("drawer-left")) {
-            drawerContent.classList.toggle("-translate-x-full");
-          }
-
-          if (drawerContent.classList.contains("drawer-right")) {
-            drawerContent.classList.toggle("translate-x-full");
-          }
-
-          if (drawerContent.classList.contains("drawer-top")) {
-            drawerContent.classList.toggle("-translate-y-full");
-          }
-
-          if (drawerContent.classList.contains("drawer-bottom")) {
-            drawerContent.classList.toggle("translate-y-full");
-          }
-          drawerContent.classList.toggle("translate-x-0");
-        }
-
-        if (drawerOverlay && drawerOverlay instanceof HTMLElement) {
-          drawerOverlay.classList.toggle("opacity-0");
-        }
-      }, 20);
-    }
-  };
 }
