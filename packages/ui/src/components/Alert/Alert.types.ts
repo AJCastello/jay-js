f319254ca72777be93f1d4effa5f7af5bc340fa4
AJ Castello <ajcastello1@gmail.com>
2023-12-17 17:07:51 -0400

    move severity splitted type

--- packages/ui/src/components/Alert/Alert.types.ts
+++ packages/ui/src/components/Alert/Alert.types.ts
@@ -1,7 +1,5 @@
 import { TBase, TBaseTagMap } from "../Base/Base.types.js";
 
-type AlertSeverity = "alert-error" | "alert-warning" | "alert-info" | "alert-success";
-
 export type TAlert<T extends TBaseTagMap> = {
-  severity?: AlertSeverity;
+  severity?: "alert-error" | "alert-warning" | "alert-info" | "alert-success";
 } & TBase<T>;
