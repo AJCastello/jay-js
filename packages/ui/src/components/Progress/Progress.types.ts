de5a4e8b46fddd4d3eb9b4e7df8d31d6d543f53c
AJ Castello <ajcastello1@gmail.com>
2024-01-22 08:39:56 -0400

    developer

--- packages/ui/src/components/Progress/Progress.types.ts
+++ packages/ui/src/components/Progress/Progress.types.ts
@@ -1,14 +1,5 @@
 import { TBase, TBaseTagMap } from "../Base/Base.types.js";
 
-type ProgressColor =
-  | "progress-primary"
-  | "progress-secondary"
-  | "progress-accent"
-  | "progress-info"
-  | "progress-success"
-  | "progress-warning"
-  | "progress-error";
-
 export type TProgress<T extends TBaseTagMap> = {
-  color?: ProgressColor;
+  color?: "progress-primary" | "progress-secondary" | "progress-accent" | "progress-info" | "progress-success" | "progress-warning" | "progress-error";
 } & TBase<T>;
