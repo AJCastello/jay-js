385c85b7727555fa9ebb247d5285cd7d18de8b33
AJCastello <ajcastello1@gmail.com>
2024-06-01 19:17:00 -0400

    chore: Update downloadTemplateFiles.ts to use degit for cloning templates

--- packages/cli/src/commands/init/services/downloadTemplateFiles.ts
+++ packages/cli/src/commands/init/services/downloadTemplateFiles.ts
@@ -1,11 +1,11 @@
 import fs from "fs-extra";
 import path from "path";
-import { execSync } from "child_process";
 import chalk from "chalk";
 import os from "os";
+import { face } from "../../../utils/terminal";
+import degit from "degit";
 
 export async function downloadTemplateFiles(templateId: string, projectName: string) {
-  const repoUrl = "https://github.com/AJCastello/jay-js.git";
   const templatePath = `templates/${templateId}`;
   const tempDir = path.join(os.tmpdir(), projectName);
 
@@ -13,22 +13,68 @@ export async function downloadTemplateFiles(templateId: string, projectName: str
     fs.removeSync(tempDir);
   }
 
-  console.log(chalk.bold(`\nCreating project: ${projectName}`));
-  console.log(chalk.bold(`Template selected: ${templateId}`));
-
   try {
     fs.ensureDirSync(projectName);
-    console.log(chalk.green(`Directory ${projectName} created successfully.`));
+    face.setMessage(`Cloning template (${templateId})...`);
 
-    execSync(`git clone --depth 1 --filter=blob:none --sparse ${repoUrl} "${tempDir}"`);
-    execSync(`cd "${tempDir}" && git sparse-checkout set "${templatePath}"`);
+    const emitter = degit(`AJCastello/jay-js/${templatePath}`, {
+      cache: true,
+      force: true,
+      verbose: true,
+    });
 
-    const fullPath = path.join(tempDir, templatePath);
-    fs.copySync(fullPath, projectName);
+    emitter.on("info", info => {
+      face.setMessage(info.message.substring(0, 50) + "...");      
+    });
+
+    const projectPath = path.join(process.cwd(), projectName)
+    await emitter.clone(projectPath);
+
     fs.removeSync(tempDir);
-
-    console.log(chalk.green.bold(`\nProject ${projectName} has been successfully set up!`));
   } catch (err) {
     console.error(chalk.red(`Error setting up the project: ${err}`));
   }
 }
+
+
+// import fs from "fs-extra";
+// import path from "path";
+// import chalk from "chalk";
+// import { face } from "../../../utils/terminal";
+
+// export async function downloadTemplateFiles(templateId: string, projectName: string) {
+//   try {
+//     async function downloadRepository(
+//       dirPath: string,
+//       targetDir: string
+//     ) {
+//       const baseUrl = `https://api.github.com/repos/AJCastello/jay-js/contents/templates/${dirPath}`;
+//       const response = await fetch(baseUrl);
+//       if (response.ok) {
+//         const items: any[] = await response.json();
+//         for (const item of items) {
+//           if (item.type === "file") {
+//             const fileUrl = item.download_url;
+//             const fileName = item.name;
+//             const arrayBuffer = await (await fetch(fileUrl)).arrayBuffer();
+//             const buffer = Buffer.from(arrayBuffer);
+//             face.setMessage(`File created: ${path.join(targetDir, fileName)}`);
+//             fs.writeFileSync(path.join(targetDir, fileName), buffer);
+//           } else if (item.type === "dir") {
+//             const subDirPath = path.join(dirPath, item.name);
+//             const subTargetDir = path.join(targetDir, item.name);
+//             face.setMessage(`Directory created: ${subTargetDir}`);
+//             fs.mkdirSync(subTargetDir, { recursive: true });
+//             await downloadRepository(subDirPath, subTargetDir);
+//           }
+//         }
+//       } else {
+//         throw new Error(`Error downloading repository: ${response.statusText}`);
+//       }
+//     }
+//     const targetDir = path.join(process.cwd(), projectName);
+//     await downloadRepository(templateId, targetDir);
+//   } catch (err) {
+//     console.error(chalk.red(`Error setting up the project: ${err}`));
+//   }
+// }
