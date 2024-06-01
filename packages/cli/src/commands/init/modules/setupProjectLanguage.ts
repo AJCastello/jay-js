f9d439a391238efbebc2346dfc0cd4320cddd4ba
AJCastello <ajcastello1@gmail.com>
2024-06-01 19:14:47 -0400

    chore: Remove unnecessary lines and update build scripts

--- packages/cli/src/commands/init/modules/setupProjectLanguage.ts
+++
@@ -1,8 +0,0 @@
-import { IJayJSCLIInitOptions } from "../types/index.js";
-
-export function setupProjectLanguage(options: IJayJSCLIInitOptions) {
-  // const projectRoot = `./${options.projectName}`;
-  // if (options.languageType === "multi") {
-  //   createDirectory(`${projectRoot}/src/locales`);
-  // } 
-}
