a2fb5a5aadbe6363d1f571cbfc25c44e3b54e80c
AJCastello <ajcastello1@gmail.com>
2024-06-01 19:13:18 -0400

    chore: Update CLI initialization process and add installDependencies function

--- packages/cli/src/commands/init/index.ts
+++ packages/cli/src/commands/init/index.ts
@@ -1,29 +1,26 @@
-// node
-import { execSync } from "node:child_process";
-
 // modules
 import { IJayJSCLIInitOptions } from "./types/index.js";
 import { setupProjectStructure } from "./modules/setupProjectStructure.js";
 import { setupBuildTools } from "./modules/setupBuildTools.js";
 import { setupJSXConfig } from "./modules/setupJSXConfig.js";
 import { setupProjectType } from "./modules/setupProjectType.js";
-import { setupProjectLanguage } from "./modules/setupProjectLanguage.js";
 import { setupCloneTemplate } from "./modules/setupCloneTemplate.js";
 import { setupUIPackage } from "./modules/setupUIPackage.js";
 import { finalizeInitialization } from "./modules/finalizeInitialization.js";
+import { face } from "../../utils/terminal.js";
+import chalk from "chalk";
+import { installDependencies } from "./modules/installDependencies.js";
 
 export async function init(options: IJayJSCLIInitOptions) {
+  face.startProgress(`Starting "${options.projectName}" project setup...`);
   await setupCloneTemplate(options);
   await setupProjectStructure(options);
   await setupBuildTools(options);
   await setupUIPackage(options);
   await setupProjectType(options);
-  setupProjectLanguage(options);
   setupJSXConfig(options);
   await finalizeInitialization(options);
-  if (options.installDependencies) {
-    process.chdir(options.projectName);
-    execSync(`pnpm install`, { stdio: "inherit" });
-    process.chdir("..");
-  }
+  face.endProgress();
+  console.log(chalk.green(`✔ Project "${options.projectName}" has been successfully set up!`));
+  installDependencies(options);
 }
