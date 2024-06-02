d032f447b356db4d21671e606a5d1f01a6c490a9
AJCastello <ajcastello1@gmail.com>
2024-06-02 11:58:40 -0400

    fix: Update import paths for i18n types in en-us.ts files

--- templates/spa-jsx-multi/src/pages/about.jsx
+++ templates/spa-jsx-multi/src/pages/about.jsx
@@ -3,7 +3,7 @@ import { i18n } from "../locales/i18n";
 export function About() {
   return (
     <section>
-      {i18n("This is your lazy loaded About page."")}
+      {i18n("This is your lazy loaded About page.")}
     </section>
   )
 }
