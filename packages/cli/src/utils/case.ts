ec74a3889d4e1c6eafda7cb7ae882dccdca403a3
AJCastello <ajcastello1@gmail.com>
2024-06-03 13:39:18 -0400

    feat: Add case conversion utility functions

---
+++ packages/cli/src/utils/case.ts
@@ -0,0 +1,25 @@
+export function toCamelCase(input: string): string {
+  return input
+    .toLowerCase()
+    .replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''))
+    .replace(/^(.)/, (c) => c.toLowerCase());
+}
+
+export function toPascalCase(input: string): string {
+  return input
+    .toLowerCase()
+    .replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''))
+    .replace(/^(.)/, (c) => c.toUpperCase());
+}
+
+export function toSnakeCase(input: string): string {
+  return input
+    .toLowerCase()
+    .replace(/[-_\s]+/g, '_');
+}
+
+export function toKebabCase(input: string): string {
+  return input
+    .toLowerCase()
+    .replace(/[-_\s]+/g, '-');
+}
