659c5db718b8278447690d5f902a5b109ef7e0f7
AJCastello <ajcastello1@gmail.com>
2024-06-07 15:44:37 -0400

    feat: Add init functionality to the runner

--- packages/cli/src/commands/run/modules/cliRunner.ts
+++ packages/cli/src/commands/run/modules/cliRunner.ts
@@ -1,9 +1,8 @@
+import { init } from "../../init/action/initAction";
 import { IJayJsRunnerOptions } from "../types";
 
-export function cliRunner(options: IJayJsRunnerOptions){
-
+export async function cliRunner(options: IJayJsRunnerOptions){
   
-
-
+  options.init && await init(options.init);
 
 }
