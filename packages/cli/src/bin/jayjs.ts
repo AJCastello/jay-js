9e4c0ecd64a352ae7965a5d794a70b8a0598bf9e
AJCastello <ajcastello1@gmail.com>
2024-06-06 20:47:58 -0400

    refactor: Register run and module command

--- packages/cli/src/bin/jayjs.ts
+++ packages/cli/src/bin/jayjs.ts
@@ -4,18 +4,22 @@
 import { Command } from "commander";
 
 // registers
+import { registerRunCommand } from "../commands/run/register/registerRunCommand.js";
 import { registerInitCommand } from "../commands/init/register/registerInitCommand.js";
 import { registerPrepareCommand } from "../commands/prepare/register/registerPrepareCommand.js";
 import { registerBuildCommand } from "../commands/render/register/registerBuildCommand.js";
 import { registerContextCommand } from "../commands/context/register/registerContextCommand.js";
 import { registerI18nCommand } from "../commands/i18n/register/registerI18nCommand.js";
+import { registerModuleCommand } from "../commands/module/register/registerModuleCommand.js";
 
 const program = new Command();
 
+registerRunCommand(program);
 registerInitCommand(program);
 registerPrepareCommand(program);
 registerBuildCommand(program);
 registerContextCommand(program);
 registerI18nCommand(program);
+registerModuleCommand(program);
 
 program.parse(process.argv);
