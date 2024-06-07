26917d09fe46a6159157b7e6ef238e0b7534da5b
AJCastello <ajcastello1@gmail.com>
2024-06-07 15:45:26 -0400

    refactor: Update runner options interface to use JayJSCLIInitOptions

--- packages/cli/src/commands/run/types/index.ts
+++ packages/cli/src/commands/run/types/index.ts
@@ -1,39 +1,5 @@
+import { IJayJSCLIInitOptions } from "../../init/types";
+
 export interface IJayJsRunnerOptions {
-  init: IInit;
-  ctx: ICtx;
-}
-
-export interface ICtx {
-  name: string;
-  resources: IResources;
-}
-
-export interface IResources {
-  actions: IAction[];
-  states: IAction[];
-}
-
-export interface IAction {
-  name: string;
-  commit: string;
-}
-
-export interface IInit {
-  name: string;
-  variant: string;
-  builder: string;
-  type: string;
-  i18n: boolean;
-  jsx: boolean;
-  ui: IUi;
-  manager: IManager;
-}
-
-export interface IManager {
-  install: string;
-}
-
-export interface IUi {
-  type: string;
-  plugin: string;
+  init: IJayJSCLIInitOptions;
 }
