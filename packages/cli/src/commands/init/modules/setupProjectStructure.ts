efdac520f10d4c0cf5e2dbb5ce57d6fdcd500358
AJCastello <ajcastello1@gmail.com>
2024-06-12 14:50:09 -0400

    refactor: Update setupProjectStructure to use template files for generating project structure

--- packages/cli/src/commands/init/modules/setupProjectStructure.ts
+++ packages/cli/src/commands/init/modules/setupProjectStructure.ts
@@ -6,25 +6,25 @@ import {
 } from "../utils/filesystem.js";
 
 import {
-  generateGitIgnoreFileContent,
-  generateIndexHtmlContent,
-  generateLogoSVGFileContent,
-  generateNpmrFileContent,
-} from "../utils/generate.js";
-
-import {
   packageFile
 } from "../services/setupConfig.js";
 
 import { toKebabCase } from "../../../utils/case.js";
+import { gitIgnoreFile, logoSVGFile, npmrFile } from "../templates/configFiles.js";
+import { indexFile } from "../templates/indexFile.js";
+import { viteConfigFile } from "../templates/viteConfigFile.js";
 
 export async function setupProjectStructure(options: IJayJSCLIInitOptions): Promise<void> {
   const projectRoot = `./${toKebabCase(options.projectName)}`;
   packageFile.name = options.projectName;
 
   await createDirectory(`${projectRoot}/public`);
-  await createFile(`${projectRoot}/public/jayjs.svg`, generateLogoSVGFileContent());
-  await createFile(`${projectRoot}/index.html`, generateIndexHtmlContent(options.projectName, options.javascriptVariant));
-  await createFile(`${projectRoot}/.gitignore`, generateGitIgnoreFileContent());
-  await createFile(`${projectRoot}/.npmrc`, generateNpmrFileContent());
+  await createFile(`${projectRoot}/public/jayjs.svg`, logoSVGFile());
+  await createFile(`${projectRoot}/index.html`, indexFile(options.projectName, options.javascriptVariant));
+  await createFile(`${projectRoot}/.gitignore`, gitIgnoreFile());
+  await createFile(`${projectRoot}/.npmrc`, npmrFile());
+
+  if (options.buildTool === "vite") {
+    await createFile(`${projectRoot}/vite.config.js`, viteConfigFile(options));
+  }
 }
