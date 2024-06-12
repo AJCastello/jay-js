8a7308bb6c2c95338101f5f0f41e983c9d54f4fa
AJCastello <ajcastello1@gmail.com>
2024-06-12 14:14:42 -0400

    refactor: Update addStorage function to use getFileContent and writeFile utilities

--- packages/cli/src/commands/context/modules/addStorage.ts
+++ packages/cli/src/commands/context/modules/addStorage.ts
@@ -1,29 +1,13 @@
-// node 
-import fs from "fs/promises";
-import path from "node:path";
+import { getFileContent, writeFile } from "../utils";
 
-// options
-import { jayJsOptions } from "../../../options/jayJsDefineOptions";
-import { toPascalCase } from "../../../utils/case";
-
-export async function addStorage({ contextName, storage }: { contextName: string; storage: string }) {
-  const projectPath = path.join(process.cwd(), jayJsOptions.build.srcDir, "contexts", contextName);
-  const interfacesFile = path.join(projectPath, `${contextName}.interfaces.ts`);
-  const contextFile = path.join(projectPath, `${contextName}.context.ts`);
-  const interfaceContent = await fs.readFile(interfacesFile, "utf8");
-  const contextContent = await fs.readFile(contextFile, "utf8");
-
-  const interfacesContentUpdated = interfaceContent.replace(
-    `I${toPascalCase(contextName)}ContextPrivateStorage {`,
-    `I${toPascalCase(contextName)}ContextPrivateStorage {\n  ${storage}: null;`
-  );
-
+export async function addStorage(
+  contextName: string,
+  storage: string
+) {
+  const [contextFile, contextContent] = await getFileContent(contextName, "context");
   const contextContentUpdated = contextContent.replace(
     "this.storage = {",
     `this.storage = {\n      ${storage}: null,`
   );
-
-  await fs.writeFile(interfacesFile, interfacesContentUpdated);
-  await fs.writeFile(contextFile, contextContentUpdated);
-  console.log(`✔ "${storage}" storage added to context "${contextName}"!`);
+  await writeFile(contextFile, contextContentUpdated);
 }
