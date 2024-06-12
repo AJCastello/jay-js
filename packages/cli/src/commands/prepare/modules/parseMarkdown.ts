3f667982eeb7e55e647bbf687357e897bcb1a7d3
AJCastello <ajcastello1@gmail.com>
2024-06-12 15:17:12 -0400

    refactor: Update parseMarkdown.ts to use async/await for marked function

--- packages/cli/src/commands/prepare/modules/parseMarkdown.ts
+++ packages/cli/src/commands/prepare/modules/parseMarkdown.ts
@@ -1,14 +1,15 @@
 // parsers
 import matter from "gray-matter";
 import { marked } from "marked";
+import { log } from "../../../utils/terminal";
 
-export function parseMarkdown(src: string): any {
+export async function parseMarkdown(src: string): Promise<Record<string, any>>{
   try {
     const { data, content } = matter(src);
-    const htmlContent = marked(content);
+    const htmlContent = await marked(content);
     return { ...data, content: htmlContent };
   } catch (error) {
-    console.error("Failed to parse markdown", error);
+    log`Failed to parse markdown: ${error}`;
     return { content: "" };
   }
 }
