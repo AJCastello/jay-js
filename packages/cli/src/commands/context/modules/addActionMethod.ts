e70d93f2cba9d6fab299ceedfe970687f3f8e900
AJCastello <ajcastello1@gmail.com>
2024-06-12 13:22:21 -0400

    feat: Add addActionMethod function to context modules

--- packages/cli/src/commands/context/modules/addActionMethod.ts
+++ packages/cli/src/commands/context/modules/addActionMethod.ts
@@ -1,28 +1,16 @@
-// node 
-import fs from "fs/promises";
-import path from "node:path";
+import { descriptionTemplate } from "../templates/description";
+import { getFileContent, writeFile } from "../utils";
 
-// options
-import { jayJsOptions } from "../../../options/jayJsDefineOptions";
-import { toPascalCase } from "../../../utils/case";
-
-export async function addActionMethod({ contextName, action }: { contextName: string; action: string }) {
-  const projectPath = path.join(process.cwd(), jayJsOptions.build.srcDir, "contexts", contextName);
-  const interfacesFile = path.join(projectPath, `${contextName}.interfaces.ts`);
-  const actionsFile = path.join(projectPath, `${contextName}.actions.ts`);
-  const interfaceContent = await fs.readFile(interfacesFile, "utf8");
-  const actionContent = await fs.readFile(actionsFile, "utf8");
-
-  const interfacesContentUpdated = interfaceContent.replace(
-    `I${toPascalCase(contextName)}ContextActions {`,
-    `I${toPascalCase(contextName)}ContextActions {\n  ${action}: () => void;`
-  );
+export async function addActionMethod(
+  contextName: string,
+  action: string,
+  description?: string
+) {
+  const [actionsFile, actionContent] = await getFileContent(contextName, "actions");
+  const descriptionComment = descriptionTemplate(description || action);
   const actionContentUpdated = actionContent.replace(
     "/** jayjs:actions */",
-    `/** jayjs:actions */\n\n  ${action}() {}`
+    `/** jayjs:actions */\n\n  ${descriptionComment}\n  ${action}() {}`
   );
-
-  await fs.writeFile(interfacesFile, interfacesContentUpdated);
-  await fs.writeFile(actionsFile, actionContentUpdated);
-  console.log(`✔ "${action}" action method added to context "${contextName}"!`);
+  await writeFile(actionsFile, actionContentUpdated);
 }
