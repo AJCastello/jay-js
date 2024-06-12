e1d0b49d1748ab1bf2f0662250cfb9790546bfc1
AJCastello <ajcastello1@gmail.com>
2024-06-12 14:50:40 -0400

    refactor: Remove unused code in setupProjectType

--- packages/cli/src/commands/init/modules/setupProjectType.ts
+++ packages/cli/src/commands/init/modules/setupProjectType.ts
@@ -1,15 +1,8 @@
-import { toKebabCase } from "../../../utils/case.js";
 import { packageFile, packageVersion } from "../services/setupConfig.js";
 import { IJayJSCLIInitOptions } from "../types/index.js";
-import { createFile } from "../utils/filesystem.js";
-import { generateViteConfigFileContent } from "../utils/generate.js";
 
 export async function setupProjectType(options: IJayJSCLIInitOptions) {
-  const projectRoot = `./${toKebabCase(options.projectName)}`;
   if (options.type === "static") {
     packageFile.dependencies["@jay-js/static"] = packageVersion["@jay-js/static"];
-    if (options.buildTool === "vite") {
-      await createFile(`${projectRoot}/vite.config.js`, generateViteConfigFileContent());
-    }
   }
 }
