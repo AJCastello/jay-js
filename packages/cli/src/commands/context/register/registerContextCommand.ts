ae6193a7489a4b9e4354ea057a8b84d53f09419e
AJCastello <ajcastello1@gmail.com>
2024-06-12 14:37:34 -0400

    feat: Add option to add description to the method in registerContextCommand

--- packages/cli/src/commands/context/register/registerContextCommand.ts
+++ packages/cli/src/commands/context/register/registerContextCommand.ts
@@ -10,8 +10,9 @@ export function registerContextCommand(program: Command) {
     .alias("context")
     .description("Create a structured context for your project")
     .option("-s, --state <state>", "Create a method to manage some private state")
-    .option("-p, --private <contextState>", "Create a new private state")
+    .option("-p, --private-state <contextState>", "Create a new private state")
     .option("-a, --action <action>", "Create a method to manage some action")
     .option("-t, --storage <storage>", "Create a method to manage some storage")
+    .option("-d, --description <description>", "Add a description to the method")
     .action(contextAction);
 }
