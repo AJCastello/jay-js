2c1ceeec3a72c7b44ba4d9ffc37c9135c394a0c4
AJCastello <ajcastello1@gmail.com>
2024-06-12 15:17:50 -0400

    refactor: Update IJayJsRunnerOptions interface to include context and module properties

--- packages/cli/src/commands/run/types/index.ts
+++ packages/cli/src/commands/run/types/index.ts
@@ -2,4 +2,41 @@ import { IJayJSCLIInitOptions } from "../../init/types";
 
 export interface IJayJsRunnerOptions {
   init: IJayJSCLIInitOptions;
+  context: {
+    name: string;
+    actions: [
+      {
+        name: string,
+        description: string
+      }
+    ];
+    states: [
+      {
+        name: string,
+        description: string
+      }
+    ];
+  };
+  module: {
+    name: string;
+    service: [
+      {
+        name: string,
+        description: string
+      }
+    ];
+    repository: [
+      {
+        name: string,
+        description: string
+      }
+    ];
+    http: [
+      {
+        name: string,
+        description: string
+      }
+    ];
+    
+  };
 }
