ced985087e928b8924d07848769d934b196040f1
AJCastello <ajcastello1@gmail.com>
2024-06-12 14:42:19 -0400

    refactor: Update contextFileTemplate to support optional storage parameter

--- packages/cli/src/commands/context/templates/contextFile.ts
+++ packages/cli/src/commands/context/templates/contextFile.ts
@@ -1,38 +1,42 @@
 import { toPascalCase } from "../../../utils/case";
+import { filterFormat } from "../utils";
 
-export function contextFileTemplate(contextName: string){
+export function contextFileTemplate(contextName: string, storage: boolean){
   const formattedName = toPascalCase(contextName);
+
+  const interfaceImports = filterFormat(
+    `I${formattedName}Context`,
+    `I${formattedName}ContextPrivateStates`,
+    `I${formattedName}ContextStates`,
+    `I${formattedName}ContextActions`,
+    storage && `I${formattedName}ContextPrivateStorage`
+  ).join(",\n  ");
+
+  const properties = filterFormat(
+    `private privateStates: I${formattedName}ContextPrivateStates`,
+    storage && `private storage: I${formattedName}ContextPrivateStorage`,
+    `states: I${formattedName}ContextStates`,
+    `actions: I${formattedName}ContextActions`
+  ).join(";\n  ");
+
   return `import { State } from "@jay-js/system";
 
 // interfaces
-import {
-  I${formattedName}Context,
-  I${formattedName}ContextPrivateStates,
-  I${formattedName}ContextPrivateStorage,
-  I${formattedName}ContextStates,
-  I${formattedName}ContextActions,
-} from "./${contextName}.interfaces";
+import {\n  ${interfaceImports},\n} from "./${contextName}.interfaces";
 
 // dependencies
 import { ${formattedName}States } from "./${contextName}.states";
 import { ${formattedName}Actions } from "./${contextName}.actions";
 
 class ${formattedName}Context implements I${formattedName}Context {
-  private contextStates: I${formattedName}ContextPrivateStates;
-  private storage: I${formattedName}ContextPrivateStorage;
-
-  states: I${formattedName}ContextStates;
-  actions: I${formattedName}ContextActions;
+  ${properties}
 
   constructor() {
-    this.contextStates = {
+    this.privateStates = {
     };
-    
-    this.storage = {
-    };
-
-    this.states = new ${formattedName}States(this.contextStates);
-    this.actions = new ${formattedName}Actions(this.states, this.storage);
+    ${storage ? `\n    this.storage = {\n    };\n` : ""}
+    this.states = new ${formattedName}States(this.privateStates);
+    this.actions = new ${formattedName}Actions(this.states${storage ? ", this.storage" : ""});
   }
 }
 
