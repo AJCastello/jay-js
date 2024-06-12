e65ae80a49932b186061de1fc2aa500bd8d89423
AJCastello <ajcastello1@gmail.com>
2024-06-12 15:17:26 -0400

    refactor: Update transformMarkdownFile.ts to use async/await for parseMarkdown function

--- packages/cli/src/commands/prepare/modules/transformMarkdownFile.ts
+++ packages/cli/src/commands/prepare/modules/transformMarkdownFile.ts
@@ -10,7 +10,7 @@ import { parseMarkdown } from "./parseMarkdown";
 
 export async function transformMarkdownFile(filePath: string) {
   const markdownContent = await fs.readFile(filePath, "utf8");
-  const transformedContent = parseMarkdown(markdownContent);
+  const transformedContent = await parseMarkdown(markdownContent);
   const transformedJsContent = `export default ${JSON.stringify(transformedContent, null, 2)};`;
   const newFilePath = filePath.replace(/\.mdx?$/, ".js");
   await fs.writeFile(newFilePath, transformedJsContent);
