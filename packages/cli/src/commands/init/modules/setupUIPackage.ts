fb6879f1399d63148b742d530026879047a796dd
AJCastello <ajcastello1@gmail.com>
2024-06-12 14:51:06 -0400

    chore: Update setupUIPackage to use template files for generating UI package structure

--- packages/cli/src/commands/init/modules/setupUIPackage.ts
+++ packages/cli/src/commands/init/modules/setupUIPackage.ts
@@ -1,14 +1,16 @@
 import { toKebabCase } from "../../../utils/case.js";
 import { packageFile, packageVersion } from "../services/setupConfig.js";
+import { cssFile } from "../templates/cssFile.js";
+import { postCSSFile } from "../templates/postCssFile.js";
+import { tailwindConfigFile } from "../templates/tailwindConfigFile.js";
 import { IJayJSCLIInitOptions } from "../types/index.js";
 import { createDirectory, createFile } from "../utils/filesystem.js";
-import { generatePostCSSFileContent, generateStyleFileContent, generateTailwindConfigFileContent } from "../utils/generate.js";
 
 export async function setupUIPackage(options: IJayJSCLIInitOptions) {
   const projectRoot = `./${toKebabCase(options.projectName)}`;
   await createDirectory(`${projectRoot}/src/styles`);
   if (options.uiPackage) {
-    await createFile(`${projectRoot}/src/styles/globals.css`, generateStyleFileContent());
+    await createFile(`${projectRoot}/src/styles/globals.css`, cssFile());
     packageFile.devDependencies.tailwindcss = packageVersion.tailwindcss;
     packageFile.devDependencies.postcss = packageVersion.postcss;
     packageFile.devDependencies.autoprefixer = packageVersion.autoprefixer;
@@ -18,8 +20,8 @@ export async function setupUIPackage(options: IJayJSCLIInitOptions) {
       packageFile.devDependencies.daisyui = packageVersion.daisyui;
     }
 
-    await createFile(`${projectRoot}/tailwind.config.js`, generateTailwindConfigFileContent(options.uiPackage, options.cssPlugin));
-    await createFile(`${projectRoot}/postcss.config.js`, generatePostCSSFileContent());
+    await createFile(`${projectRoot}/tailwind.config.js`, tailwindConfigFile(options.uiPackage, options.cssPlugin));
+    await createFile(`${projectRoot}/postcss.config.js`, postCSSFile());
     return;
   }
   await createFile(`${projectRoot}/src/styles/globals.css`, "");
