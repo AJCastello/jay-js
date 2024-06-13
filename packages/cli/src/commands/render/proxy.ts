34cd4156eb9e85f0b1deb904b3b5420f153a0554
AJCastello <ajcastello1@gmail.com>
2024-06-13 11:57:29 -0400

    refactor: Remove unused files related to rendering and preparing content

--- packages/cli/src/commands/render/proxy.ts
+++
@@ -1,8 +0,0 @@
-const queryParams = new URL(import.meta.url).searchParams;
-const originalPath = queryParams.get("path");
-
-if (originalPath === null) {
-  throw new Error("'path' query parameter is required");
-}
-
-export default originalPath;
