ceb3077ceae202b6d0f1f7438c5495c0dfe73b2e
AJCastello <ajcastello1@gmail.com>
2024-06-13 11:54:25 -0400

    refactor: Update build command to compile to static files

--- packages/cli/src/commands/build/register/registerBuildCommand.ts
+++ packages/cli/src/commands/build/register/registerBuildCommand.ts
@@ -6,6 +6,6 @@ export function registerBuildCommand(program: Command) {
     .command("build")
     .description("Build content for static site generation")
     .option("-p, --prepare", "Prepare static content")
-    .option("-s, --static", "Prepare static content")
+    .option("-s, --static", "Compile to static files")
     .action(buildAction);
 }
