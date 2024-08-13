1c102303a7a12f14713e7090422da3bc21f977bb
AJCastello <ajcastello1@gmail.com>
2024-08-12 21:44:45 -0400

    feat: Add render and query utility functions

--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -4,3 +4,6 @@ export * from "./router/index.js";
 export * from "./state/index.js";
 export * from "./i18n/index.js";
 export * from "./theme/index.js";
+export { render } from "./utils/render.js";
+export * from "./utils/query.js";
+
