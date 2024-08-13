b7def39a38e6a7ff34967408f549695c65362fe9
AJCastello <ajcastello1@gmail.com>
2024-08-12 21:46:06 -0400

    refactor: Update AllPaths type to handle null values in i18n/types/index.ts

--- packages/system/src/i18n/types/index.ts
+++ packages/system/src/i18n/types/index.ts
@@ -1,24 +1,22 @@
-type Join<K, P> = K extends string | number
+type Join<K, P> = K extends string | null
   ? P extends string | number
-    ? `${K}${"" extends P ? "" : "."}${P}`
-    : never
+  ? `${K extends string ? `${K}.` : ""}${P}`
+  : never
   : never;
 
-export type AllPaths<T, Prefix extends string = ""> = T extends object
+export type AllPaths<T, Prefix extends string | null = null> = T extends object
   ? {
-      [K in keyof T]-?: Join<Prefix, K> | AllPaths<T[K], Join<Prefix, K>>;
-    }[keyof T]
-  : "";
+    [K in keyof T]-?: Join<Prefix, K> | AllPaths<T[K], Join<Prefix, K>>;
+  }[keyof T]
+  : T extends string ? "" : any; 
 
 export type GetTypeAtPath<T, Path extends string> = Path extends keyof T
   ? T[Path]
   : Path extends `${infer Key}.${infer Rest}`
   ? Key extends keyof T
-    ? GetTypeAtPath<T[Key], Rest>
-    : never
+  ? GetTypeAtPath<T[Key], Rest>
+  : never
   : never;
-
-
 
 export interface Ii18nLanguages {
   code: string;
