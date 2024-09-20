b97cb26bd3fb095cc71b08aa06be612b5f63b03b
AJCastello <ajcastello1@gmail.com>
2024-09-20 11:05:05 -0400

    refactor: Remove commented out code and console logs in cliRunner.ts

--- packages/cli/src/commands/run/modules/cliRunner.ts
+++ packages/cli/src/commands/run/modules/cliRunner.ts
@@ -2,11 +2,5 @@ import { init } from "../../init/action/initAction";
 import { IJayJsRunnerOptions } from "../types";
 
 export async function cliRunner(options: IJayJsRunnerOptions) {
-  console.log((options as any).ctx.resources);
-  console.log((options as any).module.resources);
-  
-  // options.init && await init(options.init);
-  // options.ctx && await init(options.init);
-  
-
+  options.init && await init(options.init); 
 }
