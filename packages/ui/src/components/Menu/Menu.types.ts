4a35a98709f5791ab8a4a996a194d927ce9f8ab6
AJCastello <ajcastello1@gmail.com>
2025-02-01 15:30:38 -0400

    feat: Add 'menu-xl' size option to TMenu component

--- packages/ui/src/components/Menu/Menu.types.ts
+++ packages/ui/src/components/Menu/Menu.types.ts
@@ -2,6 +2,6 @@ import { TBase, TBaseTagMap } from "../Base/Base.types.js";
 import { TList } from "../List/List.types.js";
 
 export type TMenu<T extends TBaseTagMap> = {
-  size?: "menu-xs" | "menu-sm" | "menu-md" | "menu-lg";
+  size?: "menu-xs" | "menu-sm" | "menu-md" | "menu-lg" | "menu-xl";
   position?: "menu-vertical" | "menu-horizontal";
 } & TList<T>;
