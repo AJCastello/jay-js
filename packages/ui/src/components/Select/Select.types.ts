47df97d30060f495799c8f5eb7e8af2e9f0a019d
AJCastello <ajcastello1@gmail.com>
2025-02-01 15:32:20 -0400

    feat: Add 'select-xl' size option to TSelect component

--- packages/ui/src/components/Select/Select.types.ts
+++ packages/ui/src/components/Select/Select.types.ts
@@ -4,7 +4,7 @@ export type TSelect<T extends TBaseTagMap> = {
   bordered?: boolean;
   ghost?: boolean;
   color?: "select-primary" | "select-secondary" | "select-accent" | "select-success" | "select-warning" | "select-info" | "select-error";
-  size?: "select-lg" | "select-md" | "select-sm" | "select-xs";
+  size?: "select-xl" | "select-lg" | "select-md" | "select-sm" | "select-xs";
   label?: string;
   labelAlt?: string;
   helpers?: Array<HTMLElement>;
