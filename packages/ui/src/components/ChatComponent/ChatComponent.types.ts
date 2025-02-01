fa8fde62301796a3590024f8924627253e494aa5
AJCastello <ajcastello1@gmail.com>
2025-02-01 15:29:41 -0400

    feat: Update TChatComponent color options to include 'chat-bubble-neutral'

--- packages/ui/src/components/ChatComponent/ChatComponent.types.ts
+++ packages/ui/src/components/ChatComponent/ChatComponent.types.ts
@@ -2,5 +2,5 @@ import { TBase, TBaseTagMap } from "../Base/Base.types.js";
 
 export type TChatComponent<T extends TBaseTagMap> = {
   component?: "chat-bubble" | "chat-image" | "chat-header" | "chat-footer";
-  color?: "chat-bubble-primary" | "chat-bubble-secondary" | "chat-bubble-accent" | "chat-bubble-info" | "chat-bubble-success" | "chat-bubble-warning" | "chat-bubble-error";
+  color?: "chat-bubble-primary" | "chat-bubble-neutral" | "chat-bubble-secondary" | "chat-bubble-accent" | "chat-bubble-info" | "chat-bubble-success" | "chat-bubble-warning" | "chat-bubble-error";
 } & TBase<T>;
