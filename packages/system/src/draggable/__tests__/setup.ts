289a4c9c730ee79de7fe925c7eb61b7de9fc4990
AJCastello <ajcastello1@gmail.com>
2025-03-18 21:48:14 -0400

    feat: Add mock implementation of getBoundingClientRect for testing draggable components

---
+++ packages/system/src/draggable/__tests__/setup.ts
@@ -0,0 +1,13 @@
+Element.prototype.getBoundingClientRect = function() {
+  return {
+    width: 120,
+    height: 120,
+    top: 0,
+    left: 0,
+    bottom: 120,
+    right: 120,
+    x: 0,
+    y: 0,
+    toJSON: () => {}
+  };
+};
