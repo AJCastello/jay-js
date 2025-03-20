c6fa337da1e5fad589c4acd61fbcf3f09f2bcd7c
AJCastello <ajcastello1@gmail.com>
2025-03-19 22:59:29 -0400

    feat: Consolidate exports from utils and remove deprecated render and query exports

--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -4,8 +4,5 @@ export * from "./router/index.js";
 export * from "./state/index.js";
 export * from "./i18n/index.js";
 export * from "./theme/index.js";
-export { render } from "./utils/render.js";
-export * from "./utils/query.js";
+export { render, selector, selectors, uniKey } from "./utils/index.js";
 export * from "./forms/index.js";
-// Mantendo temporariamente para retrocompatibilidade
-export * from "./useForm/index.js";
