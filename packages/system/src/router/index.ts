ae884e3c0f782379cca67d8d2021ca801b9d644e
AJCastello <ajcastello1@gmail.com>
2025-03-25 16:20:34 -0400

    feat: Export resolvedRoutes from configuration in router module

--- packages/system/src/router/index.ts
+++ packages/system/src/router/index.ts
@@ -1,5 +1,5 @@
 export { Router } from "./core/router.js";
 export { Navigate, beforeNavigate } from "./core/navigation/navigate.js";
-export { routerDefineOptions } from "./core/configuration.js";
+export { routerDefineOptions, resolvedRoutes } from "./core/configuration.js";
 export { getParams } from "./utils/get-params.js";
 export * from "./types.js";
