96f8e9c1db85453d6a0540bb0c07af718d7490fd
AJCastello <ajcastello1@gmail.com>
2025-09-11 08:25:19 -0400

    feat: remove commented-out language options from config

--- docs/src/locales/config.ts
+++ docs/src/locales/config.ts
@@ -9,21 +9,9 @@ export default {
 			code: "pt-br",
 			import: async () => (await import("./languages/pt-br")).default,
 		},
-		// {
-		//   code: "es-es",
-		//   import: async () => (await import("./languages/es-es")).default
-		// },
-		// {
-		//   code: "pt-pt",
-		//   import: async () => (await import("./languages/pt-pt")).default
-		// },
 		{
 			code: "en-us",
 			import: async () => (await import("./languages/en-us")).default,
-		},
-		// {
-		//   code: "fr-fr",
-		//   import: async () => (await import("./languages/fr-fr")).default
-		// }
+		}
 	],
 } as Partial<Ti18nOptions>;
