281bc87ed821ba99c4c2766d085186718dd6f207
AJCastello <ajcastello1@gmail.com>
2025-09-30 09:41:46 -0400

    feat: integrate Tailwind CSS and DaisyUI for styling support

--- inspector/src/main.ts
+++ inspector/src/main.ts
@@ -1,3 +1,4 @@
+import "./style.css";
 import { Routes } from "@/routes";
 
 Routes();
--- inspector/vite.config.js
+++ inspector/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import { jayJsInspector } from "@jay-js/inspector/plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	base: "/",
@@ -10,6 +11,7 @@ export default defineConfig({
 		},
 	},
 	plugins: [
+		tailwindcss(),
 		jayJsInspector({
 			enabled: true,
 			editor: 'code-insiders', // Seu editor
